010e664a975bea2e8d4de785b0faa7d067955932
jonathan-csm <jonathansaldanha11@gmail.com>
2026-08-30 21:23:18 +0400

    first_project.py
    My first project which I created to display my information.

---
+++ first_project.py
@@ -0,0 +1,11 @@
+# This is my first project that I created and I start my journey from here!
+
+name = "Jonathan Ignatius Saldanha" 
+grade = 10
+location = "Dubai , United Arab Emirates"
+
+print("---------------------------------")
+print("Developer Profile:", name)
+print("Current Status: Grade", (grade)," and I study in", location)
+print("Status: Working towards creating a portfolio for myself!")
+print("---------------------------------")
